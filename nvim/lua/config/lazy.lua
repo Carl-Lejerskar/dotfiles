cdc3c61b55f9558a86f6daa1381834f306aec5df
Carl Lejerskar <carl.lejerskar@gmail.com>
2023-03-25 23:33:10 -0400

    updating some configs, apparently kitty phones home smh

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -16,6 +16,11 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+    {
+      "iamcco/markdown-preview.nvim",
+      ft = "markdown",
+      build = ":call mkdp#util#install()",
+    },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
